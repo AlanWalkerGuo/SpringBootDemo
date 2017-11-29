9ab7f9900c169878226f14678f2047f014e77d30
guosh <guoshaohua@outlook.com>
2017-11-29 17:01:47 +0800

    Demo1

--- src/main/java/com/springboot/backstage/controller/HellController.java
+++ src/main/java/com/springboot/backstage/controller/HellController.java
@@ -4,9 +4,9 @@ package com.springboot.backstage.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//@RestController相当于@Controller和@ResponseBody
+//SpringBoot提供了refult风格
+// @RestController相当于@Controller和@ResponseBody
 @RestController
-@RequestMapping("/springboot")
 public class HellController {
 
     @RequestMapping("/hello")
--- src/main/java/com/springboot/backstage/controller/SpringBootApp.java
+++ src/main/java/com/springboot/backstage/controller/SpringBootApp.java
@@ -1,10 +1,13 @@
 package com.springboot.backstage.controller;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
 
-@SpringBootApplication
+//
+@EnableAutoConfiguration
+@ComponentScan
 public class SpringBootApp {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootApp.class,args);
