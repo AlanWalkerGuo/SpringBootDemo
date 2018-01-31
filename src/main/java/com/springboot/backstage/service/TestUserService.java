50d5149fe6823cc0571087f214249cd09e04b3b0
guosh <guoshaohua@outlook.com>
2018-01-31 18:58:54 +0800

    通用mapper

---
+++ src/main/java/com/springboot/backstage/service/TestUserService.java
@@ -0,0 +1,15 @@
+package com.springboot.backstage.service;
+
+import com.springboot.backstage.entity.TestUser;
+
+import java.util.List;
+
+public interface TestUserService {
+    public int insertTestUser(TestUser testUser);
+    public int updateTestUser();
+    public int deleteTestUser();
+    public int deleteKeyTestUser();
+    public List<TestUser> selectTestUser();
+    public TestUser selectOneTestUser();
+    public List<TestUser> selectExample();
+}
